1b0641a7c290f300ee4c655d519be7c0e98cce83
khaled-74 <princek_59@yahoo.com>
2022-03-22 03:09:35 +0200

    Adding Ra's puzzle

---
+++ Assets/Scripts/Ra_Laser3/LaserBeam.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam 
+{
+    Vector3 pos, dir;
+    GameObject laserObj;
+    LineRenderer laser;
+    List<Vector3>laserIndices = new List<Vector3>();
+
+    public LaserBeam(Vector3 pos,Vector3 dir,Material material) 
+    {
+        this.laser = new LineRenderer();
+        this.laserObj = new GameObject();
+        this.laserObj.name = "Laser Beam";
+        this.pos = pos;
+        this.dir = dir;
+
+        this.laser=this.laserObj.AddComponent<LineRenderer>() as LineRenderer;
+        this.laser.startWidth = 0.1f;
+        this.laser.endWidth = 0.1f;
+        this.laser.material = material;
+        this.laser.startColor = Color.yellow;
+        this.laser.endColor = Color.yellow;
+
+        CastRay(pos, dir, laser);
+    }
+    
+    void CastRay(Vector3 pos,Vector3 dir,LineRenderer laser) 
+    {
+        laserIndices.Add(pos);
+        Ray ray = new Ray(pos, dir);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, 1))
+        {
+            CheckHit(hit,dir,laser);
+        }
+        else
+        {
+            laserIndices.Add(ray.GetPoint(100));
+            UpdateLaser();
+        }
+    }
+
+    void UpdateLaser() 
+    {
+        int count = 0;
+        laser.positionCount = laserIndices.Count;
+        foreach (Vector3 idx in laserIndices) 
+        {
+            laser.SetPosition(count, idx);
+            count++;
+        }
+    }
+    void CheckHit(RaycastHit hitInfo,Vector3 direction,LineRenderer laser) 
+    {
+        if (hitInfo.collider.gameObject.tag == "Mirror")
+        {
+            Vector3 pos = hitInfo.point;
+            Vector3 dir = Vector3.Reflect(direction, hitInfo.normal);
+            CastRay(pos, dir, laser);
+        }
+        else if (hitInfo.collider.gameObject.tag =="RA")
+        {
+            //Win condition
+             
+            laserIndices.Add(hitInfo.point);
+            UpdateLaser();
+
+        }
+        else
+        {
+            laserIndices.Add(hitInfo.point);
+            UpdateLaser();
+        }
+        
+    }
+}
