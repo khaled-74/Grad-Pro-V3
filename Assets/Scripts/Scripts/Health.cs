9c5ec624c52db7cfa8f257004d1faacf88dfb619
khaled-74 <princek_59@yahoo.com>
2022-03-30 05:43:59 +0200

    Adding Health System

--- Assets/Scripts/Scripts/Health.cs
+++ Assets/Scripts/Scripts/Health.cs
@@ -3,11 +3,11 @@ using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
     public Text healthText;
-    public Image healthBar, ringHealthBar;
-    public Image[] healthPoints;
+    public Image healthBar;//, ringHealthBar;
+   // public Image[] healthPoints;
 
-    float health, maxHealth = 100;
-    float lerpSpeed;
+    float health, maxHealth = 100f;
+    float lerpSpeed =0.1f;
 
     private void Start()
     {
@@ -23,29 +23,32 @@ public class Health : MonoBehaviour
 
         HealthBarFiller();
         ColorChanger();
+        if (Input.GetKeyDown(KeyCode.KeypadPlus)) { Heal(10f); }
+        if (Input.GetKeyDown(KeyCode.KeypadMinus)) { Damage(10f); }
     }
 
     void HealthBarFiller()
     {
-        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (health / maxHealth), lerpSpeed);
-        ringHealthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (health / maxHealth), lerpSpeed);
+        //healthBar.fillAmount = health / maxHealth;
+          healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (health / maxHealth), lerpSpeed);
+     //   ringHealthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (health / maxHealth), lerpSpeed);
 
-        for (int i = 0; i < healthPoints.Length; i++)
-        {
-            healthPoints[i].enabled = !DisplayHealthPoint(health, i);
-        }
+        //for (int i = 0; i < healthPoints.Length; i++)
+        //{
+        //    healthPoints[i].enabled = !DisplayHealthPoint(health, i);
+        //}
     }
     void ColorChanger()
     {
         Color healthColor = Color.Lerp(Color.red, Color.green, (health / maxHealth));
         healthBar.color = healthColor;
-        ringHealthBar.color = healthColor;
+      //   ringHealthBar.color = healthColor;
     }
 
-    bool DisplayHealthPoint(float _health, int pointNumber)
-    {
-        return ((pointNumber * 10) >= _health);
-    }
+    //bool DisplayHealthPoint(float _health, int pointNumber)
+    //{
+    //    return ((pointNumber * 10) >= _health);
+    //}
 
     public void Damage(float damagePoints)
     {
