603c0ce4845e65ed73e7b349fa11a2afa1ef4017
khaled-74 <princek_59@yahoo.com>
2022-05-06 15:27:10 +0200

    bullet destroy AND adding some stuff to enemy

---
+++ Assets/BulletDestroy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+     //   Debug.Log("Touch");
+    }
+
+}
+
