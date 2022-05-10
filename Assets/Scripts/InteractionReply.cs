b46f1dc59ec160720e8b7f2d107f8ef38ca9fdf4
RodainaElrashidy <78022199+RodainaElrashidy@users.noreply.github.com>
2022-05-10 21:23:05 +0200

    Dialogue
    Changes in ACT3 and interactionReply

--- Assets/Scripts/InteractionReply.cs
+++ Assets/Scripts/InteractionReply.cs
@@ -2,36 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PixelCrushers.DialogueSystem;
+using UnityEngine.SceneManagement;
 
 public class InteractionReply : Interactable
 {
     [SerializeField] private GameObject actor;
-
+    int i = 1;
+    //Scene currentScene = SceneManager.GetActiveScene();
+    //string sceneName = currentScene.name;
     public override void OnFocus()
     {
-        if(gameObject.name == "Stand")
+        switch (gameObject.name)
         {
-            PixelCrushers.DialogueSystem.DialogueManager.BarkString("Interact", gameObject.transform, actor.transform);
-        }
-        if (gameObject.name == "Stairs")
-        {
-            PixelCrushers.DialogueSystem.DialogueManager.BarkString("Interact", gameObject.transform, actor.transform);
-        }
+            case "Stand" :
+                PixelCrushers.DialogueSystem.DialogueManager.BarkString("Interact", gameObject.transform, actor.transform);
+                break;
 
-        PixelCrushers.DialogueSystem.DialogueManager.BarkString("Press C to interact with", gameObject.transform, actor.transform);
+            case "Stairs":
+                PixelCrushers.DialogueSystem.DialogueManager.BarkString("Interact", gameObject.transform, actor.transform);
+                break;
+
+            case "ISIS":
+                 PixelCrushers.DialogueSystem.DialogueManager.BarkString("Talk with", gameObject.transform, actor.transform);
+                break;
+
+            case "Ra":
+                break;
+            default:
+                PixelCrushers.DialogueSystem.DialogueManager.BarkString("Press C to interact with", gameObject.transform, actor.transform);
+                break;
+        }
+        
     }
 
     public override void OnInteract()
     {
-        if (gameObject.name == "Stand")
+        switch (gameObject.name)
         {
-            PixelCrushers.DialogueSystem.DialogueManager.StartConversation("sees puzzle", actor.transform);
+
+            case "Stand":
+                PixelCrushers.DialogueSystem.DialogueManager.StartConversation("sees puzzle", actor.transform);
+                break;
+
+            case "Stairs":
+                PixelCrushers.DialogueSystem.DialogueManager.StartConversation("RA's puzzle", actor.transform);
+                break;
+            case "ISIS":
+                if (SceneManager.GetSceneByName("ACT3").isLoaded && i == 1)
+                {
+                    PixelCrushers.DialogueSystem.DialogueManager.StartConversation("1St meeting W ISIS", actor.transform, gameObject.transform);
+                    i++;
+                }
+                else if (SceneManager.GetSceneByName("ACT3").isLoaded && i > 1)
+                {
+                    PixelCrushers.DialogueSystem.DialogueManager.StartConversation("Tried Isis again", actor.transform, gameObject.transform);
+                }
+                
+                break;
+
         }
-        if (gameObject.name == "Stairs")
-        {
-            PixelCrushers.DialogueSystem.DialogueManager.StartConversation("RA's puzzle", actor.transform);
-        }
-        PixelCrushers.DialogueSystem.DialogueManager.StartConversation("Barks", actor.transform, null, Random.Range(4, 6));
+        //if (gameObject.name == "Stand")
+        //{
+           
+        //}
+        //if (gameObject.name == "Stairs")
+        //{
+           
+        //}
+        //PixelCrushers.DialogueSystem.DialogueManager.StartConversation("Barks", actor.transform, null, Random.Range(4, 6));
     }
 
     public override void OnLoseFocus()
